b591a82437fa3e17b15e08b427ac321241d19c38
scooby-dooby-doo-w <nghoangkhanh67@gmail.com>
2025-10-10 15:10:55 +0700

    pushtest2

--- src/library/Model/bookManager.java
+++ src/library/Model/bookManager.java
@@ -4,5 +4,6 @@ import library.Model.Book;
 import java.util.*;
 
 public class bookManager {
-
+    public boolean addBook(String bookName, String genre, String author,
+                           int bookLength, String publishYear, int numberOfBook)
 }
