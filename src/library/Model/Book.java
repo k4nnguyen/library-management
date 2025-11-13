f2e5b7698b93a84be9845805d3cd6963c8f34102
k4nnguyen <grilledog296@gmail.com>
2025-11-13 11:34:17 +0700

    Update: Change Get/Set, bookID to final and bookID to fit iterator from List of Book

--- src/library/Model/Book.java
+++ src/library/Model/Book.java
@@ -1,16 +1,11 @@
 package library.Model;
 // ======================= AN =============================
 public class Book {
-    static int cnt = 1;
-    private String bookName;
-    private int bookID;
-    private String genre;
-    private String author;
-    private int bookLength;
-    private int publishYear;
-    private int numberOfBook;
+    private String bookName,genre,author;
+    private int bookLength,publishYear,numberOfBook;
+    private final int bookID;
     private Boolean available;
-
+    // Constructor
     public Book(){
         this.bookName = "";
         this.bookID = 0;
@@ -18,14 +13,14 @@ public class Book {
         this.bookLength = 0;
         this.publishYear = 0;
         this.numberOfBook = 0;
-        this.available = true;
+        this.available = false;
     }
-    public Book(String bookName, int bookID, String genre,
+    public Book(int id, String bookName, String genre,
     String author, int bookLength, int publishYear,
     int numberOfBook, Boolean available)
     {
+        this.bookID = id;
         setBookName(bookName);
-        this.bookID = cnt++;
         setGenre(genre);
         setAuthor(author);
         setBookLength(bookLength);
@@ -33,49 +28,50 @@ public class Book {
         setNumberOfBook(numberOfBook);
         setAvailable(available);
     }
-    public void setBookName(String bookName)
+    // Set and Get
+    public final void setBookName(String bookName)
     {
         if(!bookName.equals(""))
             this.bookName = bookName;
         else
             throw new IllegalArgumentException("Tên sách không được để trống!");
     }
-    public void setGenre(String genre)
+    public final void setGenre(String genre)
     {
         if(!genre.equals(""))
             this.genre = genre;
         else
             throw new IllegalArgumentException("Thể loại không được để trống!");
     }
-    public void setAuthor(String author)
+    public final void setAuthor(String author)
     {
         if(!author.equals(""))
             this.author = author;
         else
             throw new IllegalArgumentException("Tên tác giả không được để trống!");
     }
-    public void setBookLength(int bookLength)
+    public final void setBookLength(int bookLength)
     {
         if(bookLength > 0)
             this.bookLength = bookLength;
         else
             throw new IllegalArgumentException("Độ dài của sách phải là số dương");
     }
-    public void setPublishYear(int publishYear)
+    public final void setPublishYear(int publishYear)
     {
         if(publishYear > 0 && publishYear <= 2025)
             this.publishYear = publishYear;
         else
             throw new IllegalArgumentException("Năm xuất bản cần phải trong phạm vi 0 - 2025");
     }
-    public void setNumberOfBook(int numberOfBook)
+    public final void setNumberOfBook(int numberOfBook)
     {
         if(numberOfBook > 0)
             this.numberOfBook = numberOfBook;
         else
             throw new IllegalArgumentException("Số lượng sách cần là số dương");
     }
-    public void setAvailable(Boolean available)
+    public final void setAvailable(Boolean available)
     {
         if(available == true || available == false)
             this.available = available;
@@ -113,7 +109,7 @@ public class Book {
         this.available = false;
     }
     public void getInformation(){
-        String s = "ID: " + this.bookID + "\nTên sách: " + this.bookName + "\nThể loại: " + this.genre + "\nTác giả: " + this.author + "\nNăm xuất bản: " + this.publishYear +  "\nSố lượng trang: " + this.bookLength;
+        String s = "ID: " + String.format("%02d",this.bookID) + "\nTên sách: " + this.bookName + "\nThể loại: " + this.genre + "\nTác giả: " + this.author + "\nNăm xuất bản: " + this.publishYear +  "\nSố lượng trang: " + this.bookLength;
         System.out.println(s);
     }
 
